5fd1b5e32c6a6bdd0d65523426ab12db549fdd6f
dzulfikarsa <dzulfikarsaif13@gmail.com>
2023-07-12 13:52:25 +0700

    hapus isi report.txt

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
 import openpyxl
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def click_link(driver, url, anchor_text_list):
-    driver.get(url)
-
+# Fungsi untuk mengambil status URL
+def get_url_status(url):
     try:
-        found = False
-        for anchor_text in anchor_text_list:
-            elements = driver.find_elements(By.XPATH, f"//a[contains(text(), '{anchor_text}')]")
-            if elements:
-                for element in elements:
-                    element.click()
-                    print(f"Berhasil mengklik tautan '{anchor_text}' di URL: {url}")
-                    write_to_report(url)
-                found = True
-                break
-
-        if not found:
-            print(f"Tidak ada tautan dengan kata kunci yang sesuai di URL: {url}")
-            write_to_report_gagal(url)
-    except:
-        print(f"Gagal menemukan atau mengklik tautan di URL: {url}")
-        write_to_report_gagal(url)
-
-def write_to_report(url):
-    with open("file/report.txt", "a") as file:
-        file.write(f"{url}\n")
-
-def write_to_report_gagal(url):
-    with open("file/report_gagal.txt", "a") as file:
-        file.write(f"{url}\n")
+        response = requests.get(url)
+        if response.status_code == 200:
+            return '1'  # Mengembalikan '1' untuk status berhasil
+        else:
+            return 'X'  # Mengembalikan 'X' untuk status gagal
+    except requests.exceptions.RequestException:
+        return 'X'  # Mengembalikan 'X' untuk status gagal
 
 def process_excel(file_path, url_column):
     chrome_options = Options()
@@ -62,26 +45,57 @@ def process_excel(file_path, url_column):
     for row in sheet.iter_rows(min_row=2, values_only=True):
         if len(row) >= column_index:
             url = row[column_index - 1]
-            anchor_text_list = [
-                "Kuliah Gratis",
-                "KAMPUS KOMPUTER BANYAK BEASISWA",
-                "Kampus Komputer banyak beasiswa",
-                "Kampus Online di Jakarta",
-                "Kuliah gratis",
-                "Go to link",
-                "budiluhur.ac.id",
-                "https://budiluhur.ac.id"
-            ]
-            click_link(driver, url, anchor_text_list)
+            click_link(driver, url)
         else:
             print(f"Indeks kolom '{url_column}' di luar jangkauan pada baris: {row}")
 
     driver.quit()
 
+def click_link(driver, url):
+    driver.get(url)
+
+    try:
+        elements = driver.find_elements(By.XPATH, f"//a[@href='https://www.budiluhur.ac.id/']")
+        if elements:
+            for element in elements:
+                element.click()
+                print(f"Berhasil mengklik tautan di URL: {url}")
+        else:
+            print(f"Tidak ada tautan dengan URL yang sesuai di URL: {url}")
+    except:
+        print(f"Gagal menemukan atau mengklik tautan di URL: {url}")
+
+# Membaca file Excel input
+input_file_path = 'file/Link V1_24.xlsx'
+input_wb = openpyxl.load_workbook(input_file_path)
+input_sheet = input_wb.active
+
+# Membuat file Excel output
+output_wb = openpyxl.Workbook()
+output_sheet = output_wb.active
+output_sheet['A1'] = 'URL'
+output_sheet['B1'] = 'Status'
+
+# Mengklik URL dan menulis status ke file Excel output
+row_num = 2
+for row in input_sheet.iter_rows(min_row=2, values_only=True):
+    if len(row) >= 1:
+        url = row[0]
+        output_sheet.cell(row=row_num, column=1).value = url
+        status = get_url_status(url)
+        output_sheet.cell(row=row_num, column=2).value = status
+        row_num += 1
+
+# Menyimpan file Excel output
+output_file_path = 'file/Link V1_24_dzul.xlsx'
+output_wb.save(output_file_path)
+
+
 # Ubah file_path ke path file Excel yang sesuai
-file_path = 'file/dataset.xlsx'
+file_path = output_file_path
 
 # Ubah url_column ke nama kolom yang berisi URL
 url_column = 'URL'
 
+# Memanggil fungsi untuk memproses file Excel
 process_excel(file_path, url_column)
